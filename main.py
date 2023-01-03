6a97d793b682022e211cbd234f962e1c9a491ea7
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-04 00:32:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from datetime import date
 import pandas as pd
 import numpy as np
 
+#For weather
+import requests, json
+
 #Setup
-
 today = date.today()
-
+base_url = "https://api.openweathermap.org/data/2.5/weather?"
+url = base_url + "q=" + "Riga" + "&appid=" + "test"
+respone = requests.get(url)
+if response.status.code == 200:
+  data = response.json()
+  main = data['data']
+  temperature = main['temp']
+else:
+  st.write("Error")
+  
 #Page header
 st.title("Welcome to the coolest solar panel efficiency calculator this side of Equator!")
 
