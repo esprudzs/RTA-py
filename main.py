351e4839ea0d4f87491b6667f1752603ef54f762
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-03 23:59:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 #Setup
-today = datetime.today()
+to_day = datetime.today()
 
 #Page header
 st.title("Welcome to the coolest solar panel efficiency calculator this side of Equator!")
@@ -14,7 +14,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
   st.subheader("Today is ")
 with col2:
-  st.subheader(today)
+  st.subheader(to_day)
 with col3:
   st.subheader("Outside is ")
   
