4438f1f3de6a9867f4704f29d40be1012a7d1332
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-04 00:53:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,18 @@ today = date.today()
 #Page header
 st.title("Solar panel efficiency calculator")
 
+
 #Set up two columns
 col1, col2, col3, col4, col5, col6 = st.columns(6)
 with col1:
   st.subheader("Today: ")
 with col2:
   st.subheader(today)
-with col4:
+with col5:
   st.subheader("Outside: ")
   
-with col5:
+st.subheader("")
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+with col6:
   st.metric(label = "", value = "10C", delta = None)
 
