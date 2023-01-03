6fd66bfb39aaa8249fb0a9911d2458e33ac406e6
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-03 23:39:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import streamlit as st
 #Page header
 st.title("Welcome to the coolest solar panel efficiency calculator this side of Equator!")
 left_side, right_side = st.column(2)
-left_side.header("Today is ")
+
+with left_side:
+  st.header("Today is ")
+
+with right_side:
+  st.header("Outside is ")
+  
