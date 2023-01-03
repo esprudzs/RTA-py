7e7d66f11a499407a5a1ddef08b08c293cc59bf9
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-03 23:48:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from datetime import datetime
+import pandas as pd
+import numpy as np
 
 #Page header
 st.title("Welcome to the coolest solar panel efficiency calculator this side of Equator!")
-col1, col2 = st.columns(2)
 
+col1, col2 = st.columns(2)
 with col1:
-  st.subheader("Today is ")
+  st.subheader("Today is ", datetime.now())
 
 with col2:
   st.subheader("Outside is ")
