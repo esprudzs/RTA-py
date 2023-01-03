ae7f390a30e97e92724d890e82a3fb0a71ea7f6a
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-04 00:34:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ today = date.today()
 st.title("Welcome to the coolest solar panel efficiency calculator this side of Equator!")
 
 #Set up two columns
-col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
 with col1:
   st.subheader("Today is ")
 with col2:
