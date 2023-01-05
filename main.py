38988bb80d1d6f1409f42bae13361a970328e562
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-05 15:03:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -76,9 +76,10 @@ with col2:
 with col1:
   if brand != "":
     if brand == "Hyndai":
-      pcsonsouth = st.caption("Number of panels on S: " + str(southarea))# // hyndai1pcarea))
-#      pcsonnorth = st.caption("Number of panels on N: " + str(northarea // hyndai1pcarea))
-#      totalpcs = st.caption("Total number of panels: " + str(pcsonsouth + pcsonnorth))
+      if southarea != 0:
+        pcsonsouth = st.caption("Number of panels on S: " + str(southarea // hyndai1pcarea))
+  #      pcsonnorth = st.caption("Number of panels on N: " + str(northarea // hyndai1pcarea))
+  #      totalpcs = st.caption("Total number of panels: " + str(pcsonsouth + pcsonnorth))
 
       
       
