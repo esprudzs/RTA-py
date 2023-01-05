545375ecfca3f821e58606938f06f024cd78192a
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-05 17:13:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -91,12 +91,12 @@ with col1:
       if southarea != 0:
         pcsonsouth = southarea // _hyndai1pcarea                    #calculate how many pieces will fit in the area
         southkW = pcsonsouth * _hyndai1pcpower
-        energym2south = pcsonsouth * _hyndai1pcarea * _solarenegyS
+        energym2south = pcsonsouth * _hyndai1pcarea * _solarenergyS
         st.caption("Number of panels on S: " + str(pcsonsouth))
       if northarea != 0:
         pcsonnorth = northarea // _hyndai1pcarea
         northkW = pcsonnorth * _hyndai1pcpower
-        energym2north = pcsonnorth * _hyndai1pcarea * _solarenegyN
+        energym2north = pcsonnorth * _hyndai1pcarea * _solarenergyN
         st.caption("Number of panels on N: " + str(pcsonnorth))      
       totalpcs = pcsonsouth + pcsonnorth                              #add up both sides of the roof
       totalkW = southkW + northkW
@@ -104,12 +104,12 @@ with col1:
       if southarea != 0:
         pcsonsouth = southarea // _jinko1pcarea
         southkW = pcsonsouth * _jinko1pcpower
-        energysouth = pcsonsouth * _jinko1pcarea * _solarenegyS
+        energysouth = pcsonsouth * _jinko1pcarea * _solarenergyS
         st.caption("Number of panels on S: " + str(pcsonsouth))
       if northarea != 0:
         pcsonnorth = northarea // _jinko1pcarea
         northkW = pcsonnorth * _jinko1pcpower
-        energynorth = pcsonnorth * _jinko1pcarea * _solarenegyN
+        energynorth = pcsonnorth * _jinko1pcarea * _solarenergyN
         st.caption("Number of panels on N: " + str(pcsonnorth)) 
       totalpcs = pcsonsouth + pcsonnorth
       totalkW = southkW + northkW
