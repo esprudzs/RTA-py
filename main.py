91b21fa89eb190ea00bbfa7a0203db2f723aeb56
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-05 14:55:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ temperature = GetTemp(location)
 #constants
 _solarEnergyS = 1219   #expected solar energy, kWh/m2 per year, facing South
 _solarEnergyN = 554    #expected solar energy, kWh/m2 per year, facing North
+_hyndai1pcarea = 1.719 * 1.140  #Hyndai, 1 unit area, m2
+_hyndai1pcpower = 410           #Hyndai, 1 unit max output, W
+_jinko1pcarea = 1.903 * 1.134   #Jinko, 1 unit area, m2
+_jinko1pcpower = 470            #Jinko, 1 unit max output, W
 
 #Page header
 st.header("Solar panel efficiency calculator")
@@ -48,15 +52,15 @@ with col6:
 col1, col2, col3 = st.columns(3)
 
 with col1:
-  south = st.number_input("Specify your total roof area facing South in" + " $$m^2$$")
+  southarea = st.number_input("Specify your total roof area facing South in" + " $$m^2$$")
 with col2:
-  north = st.number_input("Specify your total roof area facing North in" + " $$m^2$$")
+  northarea = st.number_input("Specify your total roof area facing North in" + " $$m^2$$")
 #with col3:
 #This ugly crap is not working
 #  st.text("While most buildings are not placed along lattitude or longtitude axes, choose areas angled more towards South or North.")
 
 #Set up columns
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
   totarea = st.caption("Total area: " + str(south + north) + " $$m^2$$")
@@ -69,5 +73,9 @@ with col2:
     else:
       st.image("https://site-539722.mozfiles.com/files/539722/catitems/Jinko470-1-511f94d46e72b1fe7e62ce1c1fb6cf24.jpg?4934090")
   
-    
-  
+with col1:
+  if brand != "":
+    if brand == "Hyndai":
+      pcsonsouth = st.caption("Number of panels on S: " + str(southarea % hyndai1pcarea))
+      pcsonnorth = st.caption("Number of panels on N: " + str(northarea % hyndai1pcarea))
+      totalpcs = st.caption("Total number of panels: " + str(pcsonsouth + pcsonnorth))
