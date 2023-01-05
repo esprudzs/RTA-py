0f9ccbf9de98e5616ecd99fab83b89e684174c01
esprudzs <59663811+esprudzs@users.noreply.github.com>
2023-01-05 17:56:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -107,8 +107,10 @@ with col1:
     st.caption("Total number of panels: " + str(totalpcs))
     st.caption("Total system power: " + str("%.2f" % totalkW) + "kW")
 
-  #years = st.slider("Number of years for income calculation:", 1, 10)
-st.text = ('Hello World')
+  years = st.slider("Number of years for income calculation:", 1, 10)
+  
+with col2:
+  st.text = ('Hello World')
   
   
   
